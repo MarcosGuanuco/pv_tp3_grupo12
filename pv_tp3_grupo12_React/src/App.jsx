f3b1dc7b4107fe3ae3295a5bdc2da0f3f162a904
Santiago-Torres72 <santygithub@hotmail.com>
2026-05-15 21:39:34 -0300

    Cree los componentes Header, Nav, Footer y ListaProyectos.jsx

--- pv_tp3_grupo12_React/src/App.jsx
+++ pv_tp3_grupo12_React/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import '../src/components/Footer'
+import '../src/components/Header'
+import '../src/components/Nav'
+import '../src/components/ListaProyectos'
 
 function App() {
   const [count, setCount] = useState(0)
