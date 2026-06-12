36c6bcca2428d1d25323caaebe9eef4e9ad0b663
AmadorJuan <juanpablo243gg@gmail.com>
2026-06-12 16:05:45 -0300

    Modifique los archivos Header, Footer y sus estilos en css.

--- pv_tp3_grupo12_React/src/components/Footer.jsx
+++ pv_tp3_grupo12_React/src/components/Footer.jsx
@@ -1,13 +1,28 @@
+import unju from '../assets/unjuvirtual.png'
 import '../css/Footer.css'
-
 function Footer(){
-    
     return(
         <footer className="footer">
-            <div className="footer-container">
-                <h2>&copy; 2026 Proyecto PV</h2>
+
+            <div className="footer-grid">
+                <img src={unju} alt="programacion" className='footer_img' />
+                <h3>Trabajo Practico de Programación Visual</h3>
+                <div>
+                    <h3>Integrantes</h3>
+                    <ul>
+                        <li>Amador Juan Pablo - AmadorJuan</li>
+                        <li>Guanuco Marcos - MarcosGuanuco</li>
+                        <li>Torres Santiago - Santiago-Torres72</li>
+                    </ul>
+                </div>
+
             </div>
+
+            <div className="footer-container">
+                <p>&copy; 2026 - Proyecto ProgramacionVisual</p>
+            </div>
+
         </footer>
     )
 }
-export default Footer
+export default Footer;
