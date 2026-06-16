35f7aa4a2f07dcca77db80f1b9752ff2ef4d2dd9
MarcosGuanuco <marcos-eze12@hotmail.com>
2026-06-15 21:11:32 -0300

    Implemente el Consumo del Estado Global en el Encabezado (<Header />) consumiendo y mostrando el nombre de ususario y su rol en este

--- pv_tp3_grupo12_React/src/components/Header.jsx
+++ pv_tp3_grupo12_React/src/components/Header.jsx
@@ -1,7 +1,10 @@
-
 import '../css/Header.css'
 import react from '../assets/react.svg'
+import { useContext } from 'react'
+import { UsuarioContext } from '../context/UsuarioContext'
+
 function Header(){
+    const { usuario } = useContext(UsuarioContext);
     return(
         <header className="header">
             <img src={react} alt="programacion" className='header_img' />
@@ -10,9 +13,11 @@ function Header(){
                 <h1 className='titulo'>PROGRAMACION VISUAL</h1>
                 <p>Bienvenido a la aplicación</p>
             </div>
+            <div className="header-usuario">
+                <span>{usuario.nombre}</span> - <span>{usuario.rol}</span>
+            </div>
         </header>
     )
 }
 
 export default Header
-
